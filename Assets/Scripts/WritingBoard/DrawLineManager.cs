021f698179eb9961c9134290781ebae88cf09f35
Mohamed Sadiq Ikbal <mohamedsadiq.ikbal@gmail.com>
2021-11-04 21:40:59 +0100

    Board writing part completed and constraint to one side

--- Assets/Scripts/WritingBoard/DrawLineManager.cs
+++ Assets/Scripts/WritingBoard/DrawLineManager.cs
@@ -1,16 +1,18 @@
 using System;
 using Fusion;
+using Unity.Mathematics;
 using UnityEngine;
 
 namespace WritingBoard
 {
     public class DrawLineManager : MonoBehaviour
     {
-        [SerializeField] private Material _chalkTipMaterial;
-        [SerializeField] private Transform _tipTransform;
+        [SerializeField] private GameObject _chalkLineRendererPrefab;
         [SerializeField] private Transform _visualBoard;
         private LineRenderer _currLine;
         private int _numTouch = 0;
+
+        private Vector3 _previousPoint = Vector3.zero;
         private void OnTriggerEnter(Collider other)
         {
             if (!other.gameObject.TryGetComponent<BoardManager>(out var board)) return;
@@ -20,7 +22,7 @@ namespace WritingBoard
         private void OnTriggerStay(Collider other)
         {
             if (!other.gameObject.TryGetComponent<BoardManager>(out var board)) return;
-            DrawLine();
+            DrawLine(other);
         }
 
         private void OnTriggerExit(Collider other)
@@ -31,27 +33,32 @@ namespace WritingBoard
 
         private void InitiateLineDrawObject(Collider other)
         {
-            var go = new GameObject
-            {
-                transform =
-                {
-                    parent = _visualBoard
-                }
-            };
-
-            _currLine = go.AddComponent<LineRenderer>();
+            
+            var go = Instantiate(_chalkLineRendererPrefab,_visualBoard);
+            go.transform.position = transform.position;
+            go.transform.localRotation = Quaternion.identity;
+            _currLine = go.GetComponent<LineRenderer>();
             _currLine.startWidth = 0.005f;
             _currLine.endWidth = 0.005f;
-            _currLine.material = _chalkTipMaterial;
             _currLine.useWorldSpace = false;
             _numTouch = 0;
+
         }
 
-        private void DrawLine()
+        private void DrawLine(Collider other)
         {
+            if (_currLine == null) return;
             _currLine.positionCount = _numTouch + 1;
-            _currLine.SetPosition(_numTouch, _tipTransform.position);
+            var writePoint = _currLine.transform.InverseTransformPoint(transform.position);
+            var writePointProjected= new Vector3(writePoint.x, writePoint.y, 0); // Projecting to the board surface
+            //if (( Vector3.Distance(_previousPoint ,transform.position) < 0.001f)) return;
+            _currLine.SetPosition(_numTouch, writePointProjected);
             _numTouch++;
+            //_previousPoint = transform.position;
         }
+
     }
 }
+//TODO: Check that the point is not the previous point
+// The current commented code does the job but added origin to the points when it misses
+// TO be explored
