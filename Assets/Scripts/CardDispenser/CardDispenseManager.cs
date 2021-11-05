7fd3ab4e895c8d4f12759c57d63d3de2c4b1923e
Mohamed Sadiq Ikbal <mohamedsadiq.ikbal@gmail.com>
2021-11-05 08:22:03 +0100

    Prefab made with minor adjusted. Scanning the cards back tested and did not succeed.

--- Assets/Scripts/CardDispenser/CardDispenseManager.cs
+++ Assets/Scripts/CardDispenser/CardDispenseManager.cs
@@ -10,12 +10,12 @@ namespace CardDispenser
     {
         [SerializeField] private Transform _spawnPoint;
         [SerializeField] private float _distMovedForNextSpawn = 0.1f;
-        [SerializeField] private float _buttonThrowForce = 1f;
+        [SerializeField] private float _buttonThrowForce = 0.5f;
         private List<GameObject> _cardsList;
         private bool _isCardSlotFree;
         private GameObject _currentCardSpawned;
         private ButtonController _button;
-        
+
         public void Spawned()
         {
             var networkCardParent = GetComponentInChildren<NetworkCardParent>();
@@ -29,27 +29,28 @@ namespace CardDispenser
             _isCardSlotFree = true;
 
             _button = GetComponentInChildren<ButtonController>();
-            Debug.Log(_button.gameObject.name);
         }
 
         public override void FixedUpdateNetwork()
         {
-            if (_cardsList.Count <= 0) return;
-            if (_isCardSlotFree)
+            if (_cardsList.Count > 0)
             {
-                _currentCardSpawned = SpawnRandomCard();
+                if (_isCardSlotFree)//On grab the object becomes kinematics
+                {
+                    _currentCardSpawned = SpawnRandomCard();
+                }
+                CheckSlotAvailability();
+                PushCardOnButtonPress();
             }
-            CheckSlotAvailability();
-            PushCardOnButtonPress();
-            Debug.Log(_button.IsButtonPressed);
         }
+        
 
         private void PushCardOnButtonPress()
         {
             //Add force if button pressed
-            if (_button.IsButtonPressed)
+            if (_button.IsButtonPressed & _currentCardSpawned!=null)
             {
-                _currentCardSpawned.GetComponentInChildren<Rigidbody>().AddForce(Vector3.right * _buttonThrowForce, ForceMode.Impulse);
+                _currentCardSpawned.GetComponentInChildren<Rigidbody>().AddForce(_currentCardSpawned.transform.forward * _buttonThrowForce, ForceMode.Impulse);
             }
         }
 
@@ -62,9 +63,9 @@ namespace CardDispenser
         private GameObject SpawnRandomCard()
         {
             var cardIndex = Random.Range(0, _cardsList.Count);
-            _cardsList[cardIndex].SetActive(true);
             _cardsList[cardIndex].transform.position = _spawnPoint.position;
             _cardsList[cardIndex].transform.rotation = _spawnPoint.rotation;
+            _cardsList[cardIndex].SetActive(true);
             var currentCard = _cardsList[cardIndex];
             _cardsList.RemoveAt(cardIndex);
             _isCardSlotFree = false;
