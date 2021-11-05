d2b466c1219b544529e9557034dbef1d9cb5d3b8
Mohamed Sadiq Ikbal <mohamedsadiq.ikbal@gmail.com>
2021-11-05 05:44:41 +0100

    Card dispenser with auto refill setup with networking

---
+++ Assets/Scripts/CardDispenser/CardDispenseManager.cs
@@ -0,0 +1,54 @@
+using System;
+using System.Collections.Generic;
+using Fusion;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+namespace CardDispenser
+{
+    public class CardDispenseManager : SimulationBehaviour, ISpawned
+    {
+        [SerializeField] private Transform _spawnPoint;
+        [SerializeField] private float _distMovedForNextSpawn = 0.1f;
+        private List<GameObject> _cardsList;
+        private bool _isCardSlotFree;
+        private GameObject _currentCardSpawned;
+        
+        public void Spawned()
+        {
+            var networkCardParent = GetComponentInChildren<NetworkCardParent>();
+            var cardSet = networkCardParent.GetComponentsInChildren<Rigidbody>();
+            _cardsList = new List<GameObject>();
+            foreach (var rb in cardSet)
+            {
+                _cardsList.Add(rb.gameObject);
+                rb.gameObject.SetActive(false);
+            }
+            _isCardSlotFree = true;
+        }
+
+        public override void FixedUpdateNetwork()
+        {
+            if (_cardsList.Count <= 0) return;
+            if (_isCardSlotFree)
+            {
+                _currentCardSpawned = SpawnRandomCard();
+            }
+
+            var dist = Vector3.Distance(_currentCardSpawned.transform.position, _spawnPoint.position);
+            _isCardSlotFree = Math.Round(dist, 2) > _distMovedForNextSpawn;
+        }
+
+        private GameObject SpawnRandomCard()
+        {
+            var cardIndex = Random.Range(0, _cardsList.Count);
+            _cardsList[cardIndex].SetActive(true);
+            _cardsList[cardIndex].transform.position = _spawnPoint.position;
+            _cardsList[cardIndex].transform.rotation = _spawnPoint.rotation;
+            var currentCard = _cardsList[cardIndex];
+            _cardsList.RemoveAt(cardIndex);
+            _isCardSlotFree = false;
+            return currentCard;
+        }
+    }
+}
