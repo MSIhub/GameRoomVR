a65f2e8603e88297a33a0729a49835fff935a37a
Mohamed Sadiq Ikbal <mohamedsadiq.ikbal@gmail.com>
2021-11-05 10:01:50 +0100

    Timer clock prefab added with button to reset the clock

--- Assets/Scripts/CardDispenser/ButtonController.cs
+++ Assets/Scripts/CardDispenser/ButtonController.cs
@@ -6,6 +6,7 @@ namespace CardDispenser
     {
         [SerializeField] private Transform _buttonBase;
         [SerializeField] private Transform _buttonPress;
+        [SerializeField] private float _zOffset = 0.01f;
 
         public bool IsButtonPressed { get; set; }
 
@@ -16,14 +17,14 @@ namespace CardDispenser
         private void OnTriggerEnter(Collider other)
         {
             if (IsButtonPressed) return;
-            _buttonPress.Translate(new Vector3(0,-0.01f,0), _buttonBase);
+            _buttonPress.Translate(new Vector3(0,-_zOffset,0), _buttonBase);
             IsButtonPressed = true;
         }
 
         private void OnTriggerExit(Collider other)
         {
             if (!IsButtonPressed) return;
-            _buttonPress.Translate(new Vector3(0,0.01f,0), _buttonBase);
+            _buttonPress.Translate(new Vector3(0,_zOffset,0), _buttonBase);
             IsButtonPressed = false;
 
         }
