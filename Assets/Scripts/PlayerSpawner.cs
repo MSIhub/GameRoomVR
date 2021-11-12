0aad99bb62157bf1a3c871e3934253cf36f7df28
Mohamed Sadiq Ikbal <mohamedsadiq.ikbal@gmail.com>
2021-11-12 03:03:04 +0100

    Spawning point for each scene feature added.

--- Assets/Scripts/PlayerSpawner.cs
+++ Assets/Scripts/PlayerSpawner.cs
@@ -1,18 +1,41 @@
+using System.Collections.Generic;
 using UnityEngine;
 using Fusion;
+using UnityEngine.SceneManagement;
 
 public class PlayerSpawner : MonoBehaviour
 {
 	[SerializeField] private Player _playerPrefab;
-
+	[SerializeField] private List<GameObject> _startPrefab;
+	private Vector3 _startPosition;
+	private Quaternion _startRotation;
 	public void OnPlayerJoined(NetworkRunner runner, PlayerRef player)
 	{
-		runner.Spawn( _playerPrefab, null, null, player );
+		_startPosition = Vector3.zero;
+		_startRotation = Quaternion.identity;
+		var scene = SceneManager.GetActiveScene();
+		if (scene.buildIndex <= _startPrefab.Count)
+		{ 
+			_startPosition = _startPrefab[scene.buildIndex].transform.position; 
+			_startRotation = _startPrefab[scene.buildIndex].transform.rotation;
+		}
+		runner.Spawn( _playerPrefab, _startPosition, _startRotation, player );
 	}
 
+	
 	public void OnConnectedToServer(NetworkRunner runner)
 	{
+		_startPosition = Vector3.zero;
+		_startRotation = Quaternion.identity;
+		var scene = SceneManager.GetActiveScene();
+		if (scene.buildIndex <= _startPrefab.Count)
+		{ 
+			_startPosition = _startPrefab[scene.buildIndex].transform.position; 
+			_startRotation = _startPrefab[scene.buildIndex].transform.rotation;
+		}
 		if(runner.Topology==SimulationConfig.Topologies.Shared)
 			runner.Spawn( _playerPrefab, null, null, runner.LocalPlayer );
 	}
+	
+
 }
